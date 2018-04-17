7eddf0963da2808bbbbb6a8e19a9d07897d02ce0
Anjana Sarath <anjanasarath5@gmail.com>
2018-04-18 00:52:16 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import App from './src/js/components/App';
+import styles from './index.css';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const config = require('config');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    '@babel/polyfill',
+    './app/index.js',
+    'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        include: [__dirname],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+          }
+        }
+      },
+      {test: /\.css$/, enforce: "pre", use: ['style-loader','css-loader']},
+      {test: /\.(png|jpg|gif|svg|jpeg)$/, enforce: "pre", use:[{loader: 'url-loader',options: {limit: 25000}}]}
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8090
+  },
+  node: {
+    fs: 'empty'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: path.join(__dirname, './app/index.html')
+    }),
+    new webpack.IgnorePlugin(/^(buffertools)$/),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      ENV: config.webpack,
+      appConfig: JSON.stringify(config.clientConfig)
+    }),
+    new webpack.LoaderOptionsPlugin({ debug: process.env === 'production' })
+  ],
+  mode: 'development',
+}
