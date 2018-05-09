b1b99e1a7898037fedb6679a3be30b42586376e6
Anjana Sarath <anjanasarath5@gmail.com>
2018-05-09 20:07:04 +0530

    CSS- review background

--- app/src/components/review.js
+++ app/src/components/review.js
@@ -35,45 +35,47 @@ const UserReview = (props) => (
 );
 
 const Review = (props) => (
-  <div className='review-slider pb-10'>
-    <Slider className='bg-white' {...props.settings}>
-      <div>
-        <UserReview
-          image={User1}
-          name="Richard B"
-          rating={5}
-          location="Car park on High Holborn, London"
-          comments="Very easy. The convenient parking made our stay all the more enjoyable. Cheaper than the train and cheaper than other car parks too. Easy booking system and pre-payment takes the stress out of finding the cash or texting to pay. We will definitely use again."
-        />
-      </div>
-      <div>
-        <UserReview
-          image={User2}
-          name="Marcus F"
-          rating={5}
-          location="Driveway on Vicarage Road, Birmingham"
-          comments="Not used to reviewing parking spaces, but have to say this one was perfect! We used it as away supporters attending a Fulham game. Very convenient 10 minute walk to the ground, easy to find and a quick escape on the South Circular afterwards. Great service."
-        />
-      </div>
-      <div>
-        <UserReview
-          image={User3}
-          name="Gemma T"
-          rating={5}
-          location="Car park on Whiteladies Road, Bristol"
-          comments="Simple and easy-to-use app, perfect for my commute into work. Saves on stress of having to find a space in the morning in such a difficult area to find parking. Very happy with the service, has made my journey much easier and hope to use on a regular basis."
-        />
-      </div>
-      <div>
-        <UserReview
-          image={User4}
-          name="Jennifer M"
-          rating={5}
-          location="Car park on Melton Street, London"
-          comments="I used this space for a hospital appointment in London. The whole JustPark experience has been great! So easy to find and book a space - at a brilliant price too! Will definitely use again, saved us a bundle of money and stress. Thank you JustPark :-)"
-        />
-      </div>
-    </Slider>
+  <div className='pb-10'>
+    <div className='review-slider'>
+      <Slider {...props.settings}>
+        <div>
+          <UserReview
+            image={User1}
+            name="Richard B"
+            rating={5}
+            location="Car park on High Holborn, London"
+            comments="Very easy. The convenient parking made our stay all the more enjoyable. Cheaper than the train and cheaper than other car parks too. Easy booking system and pre-payment takes the stress out of finding the cash or texting to pay. We will definitely use again."
+          />
+        </div>
+        <div>
+          <UserReview
+            image={User2}
+            name="Marcus F"
+            rating={5}
+            location="Driveway on Vicarage Road, Birmingham"
+            comments="Not used to reviewing parking spaces, but have to say this one was perfect! We used it as away supporters attending a Fulham game. Very convenient 10 minute walk to the ground, easy to find and a quick escape on the South Circular afterwards. Great service."
+          />
+        </div>
+        <div>
+          <UserReview
+            image={User3}
+            name="Gemma T"
+            rating={5}
+            location="Car park on Whiteladies Road, Bristol"
+            comments="Simple and easy-to-use app, perfect for my commute into work. Saves on stress of having to find a space in the morning in such a difficult area to find parking. Very happy with the service, has made my journey much easier and hope to use on a regular basis."
+          />
+        </div>
+        <div>
+          <UserReview
+            image={User4}
+            name="Jennifer M"
+            rating={5}
+            location="Car park on Melton Street, London"
+            comments="I used this space for a hospital appointment in London. The whole JustPark experience has been great! So easy to find and book a space - at a brilliant price too! Will definitely use again, saved us a bundle of money and stress. Thank you JustPark :-)"
+          />
+        </div>
+      </Slider>
+    </div>
   </div>
 );
 
